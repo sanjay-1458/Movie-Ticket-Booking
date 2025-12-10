5038629130fabba6af7c30e61c5c7f4d105e2cbf
Sanjay Thakur <2147sanjay@gmail.com>
2025-12-10 22:20:43 +0530

    Fixing Image Path

--- client/src/components/HeroSection/HeroSection.tsx
+++ client/src/components/HeroSection/HeroSection.tsx
@@ -6,7 +6,7 @@ import { useNavigate } from 'react-router-dom'
 function HeroSection() {
   const navigate = useNavigate();
   return (
-    <div className='flex flex-col items-start justify-center gap-4 px-6 md:px-16 lg:px-36 bg-[url("./backgroundImage.png")] h-screen bg-cover bg-center'>
+    <div className='flex flex-col items-start justify-center gap-4 px-6 md:px-16 lg:px-36 bg-[url("/backgroundImage.png")] h-screen bg-cover bg-center'>
       <img src={assets.marvelLogo} alt="Marvel Logo" className='max-h-11 lg:h-11 mt-20'></img>
       <h1 className='text-5xl md:text-[70px] md:leading-18 font-semibold max-w-110'>Guardians<br></br>of the Galaxy</h1>
       <div className='flex items-center gap-4 text-gray-300'>
